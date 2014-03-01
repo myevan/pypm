e76929dc9eeb273e1ceaba18d24383eebd7dadbf
Song Young-jin <myevan80@gmail.com>
2014-03-01 15:30:06 +0900

    + hello_world @ examples

---
+++ examples/hello_world/manage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from pypm import ProjectManager
+
+pm = ProjectManager()
+
+@pm.command(messages=dict(type=str, nargs='+', help='echo messages'))
+def echo(messages):
+    """
+    echo messages
+    """
+    print messages
+
+if __name__ == '__main__':
+    import sys
+    pm.run_command(sys.argv[1:])
