9ed390a9de1ffbaddb8629ac63bf36bb240c357a
Song Young-jin <myevan80@gmail.com>
2014-03-09 23:18:33 +0900

    ! hello_world 실행 안되는 버그 수정 @ examples

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -5,7 +5,7 @@ pm = ProjectManager()
 
 @pm.command(messages=dict(type=str, nargs='+', help='echo messages'))
 def echo(messages):
- ', hint)   """
+    """
     echo messages
     """
     print messages
